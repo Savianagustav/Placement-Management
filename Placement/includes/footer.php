80774ce0dd798d3e7f1eacdfd99b2f00da09b8d5
Savianagustav <160801587+Savianagustav@users.noreply.github.com>
2024-02-22 06:44:17 +0530

    Add files via upload

---
+++ Placement/includes/footer.php
@@ -0,0 +1,49 @@
+<!--/footer-->
+<div class="footer">
+    <div class="footer-top">
+        <div class="col-md-4 footer-grid">
+            <h4>PLACEMENT Management System</h4>
+            <ul class="bottom">
+                <li>Designed By: Saviana & Neha</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Message Us Now</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-home"></i>Available 24/7 </li>
+                <li><i class="glyphicon glyphicon-envelope"></i><a href="suarez081119@gmail.com">placementmanagement@gmail.com</a></li>
+                <li><i class="glyphicon glyphicon-envelope"></i>0000000000</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Address Location</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-map-marker"></i>K. Narayanapura Cross, Banglore.</li>
+                <li><i class="glyphicon glyphicon-earphone"></i>Mobile: 0000000000</li>
+            </ul>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<div class="copy">
+    <p>&copy; Placement Management System </p>
+</div>
+<div class="clearfix"> </div>
+<!--//footer-->
+<!--start-smooth-scrolling-->
+<script type="text/javascript">
+    $(document).ready(function() {
+        var defaults = {
+            containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+        };
+        $().UItoTop({ easingType: 'easeOutQuart' });
+    });
+</script>
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+</body>
+</html>
